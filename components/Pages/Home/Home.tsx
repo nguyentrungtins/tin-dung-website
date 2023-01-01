042cfb4f91a6c1839d17a6caee307c7cdf6c074e
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-01-01 14:48:00 +0700

    test ssh

--- components/Pages/Home/Home.tsx
+++ components/Pages/Home/Home.tsx
@@ -12,7 +12,6 @@ const HomePage = ({ products }) => {
   return (
     <div className="px-1 pt-0 sm:px-[10%] sm:pt-[40px]">
       <Breadcrumb />
-
       <_Header />
       <FilterPanel />
       <ProductList products={products} />
